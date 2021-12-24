d2bd177a0040a06c9595882170b8f7676710209a
Eko Teguh Wicaksono <echotechno@rocketmail.com>
2021-12-25 04:39:00 +0700

    ViewModel unit test

---
+++ app/src/test/java/com/etwicaksono/academy/ui/bookmark/BookmarkViewModelTest.kt
@@ -0,0 +1,23 @@
+package com.etwicaksono.academy.ui.bookmark
+
+import org.junit.Assert.*
+import org.junit.Before
+
+import org.junit.Test
+
+class BookmarkViewModelTest {
+
+    private lateinit var viewModel: BookmarkViewModel
+
+    @Before
+    fun setup(){
+        viewModel= BookmarkViewModel()
+    }
+
+    @Test
+    fun getBookmarks() {
+        val courseEntities=viewModel.getBookmarks()
+        assertNotNull(courseEntities)
+        assertEquals(5,courseEntities.size)
+    }
+}
