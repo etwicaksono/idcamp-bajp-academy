2dff4615128d69f4338e966a16212b8b4a5bce4c
Eko Teguh Wicaksono <echotechno@rocketmail.com>
2022-01-02 17:10:08 +0700

    ADD assets

--- app/src/main/java/com/etwicaksono/academy/ui/detail/DetailCourseViewModel.kt
+++ app/src/main/java/com/etwicaksono/academy/ui/detail/DetailCourseViewModel.kt
@@ -5,23 +5,23 @@ import com.etwicaksono.academy.data.CourseEntity
 import com.etwicaksono.academy.data.ModuleEntity
 import com.etwicaksono.academy.utils.DataDummy
 
-class DetailCourseViewModel: ViewModel() {
+class DetailCourseViewModel : ViewModel() {
     private lateinit var courseId: String
 
-    fun setSelectedCourse(courseId:String){
-        this.courseId=courseId
+    fun setSelectedCourse(courseId: String) {
+        this.courseId = courseId
     }
 
-    fun getCourse():CourseEntity{
-        lateinit var course:CourseEntity
-        val courseEntities=DataDummy.generateDummyCourse()
-        for(courseEntity in courseEntities){
-            if(courseEntity.courseId == courseId){
-                course=courseEntity
+    fun getCourse(): CourseEntity {
+        lateinit var course: CourseEntity
+        val courseEntities = DataDummy.generateDummyCourse()
+        for (courseEntity in courseEntities) {
+            if (courseEntity.courseId == courseId) {
+                course = courseEntity
             }
         }
         return course
     }
 
-    fun getModules():List<ModuleEntity> =DataDummy.generateDummyModules(courseId)
+    fun getModules(): List<ModuleEntity> = DataDummy.generateDummyModules(courseId)
 }
