7d860e6cf352813ab09359c18225e28cf9d08893
Eko Teguh Wicaksono <echotechno@rocketmail.com>
2022-01-08 08:32:50 +0700

    update ui test

--- app/src/androidTest/java/com/etwicaksono/academy/ui/home/HomeActivityTest.kt
+++ app/src/androidTest/java/com/etwicaksono/academy/ui/home/HomeActivityTest.kt
@@ -12,21 +12,33 @@ import com.etwicaksono.academy.utils.DataDummy
 import org.junit.Rule
 import org.junit.Test
 
-class HomeActivityTest{
+class HomeActivityTest {
     private val dummyCourse = DataDummy.generateDummyCourses()
 
     @get:Rule
-    var activityRule=ActivityScenarioRule(HomeActivity::class.java)
+    var activityRule = ActivityScenarioRule(HomeActivity::class.java)
 
     @Test
-    fun loadCourses(){
+    fun loadCourses() {
+        delayTwoSeconds()
         onView(withId(R.id.rv_academy)).check(matches(isDisplayed()))
-        onView(withId(R.id.rv_academy)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dummyCourse.size))
+        onView(withId(R.id.rv_academy)).perform(
+            RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
+                dummyCourse.size
+            )
+        )
     }
 
     @Test
-    fun loadDetailCourses(){
-        onView(withId(R.id.rv_academy)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0,click()))
+    fun loadDetailCourses() {
+        delayTwoSeconds()
+        onView(withId(R.id.rv_academy)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                0,
+                click()
+            )
+        )
+        delayTwoSeconds()
         onView(withId(R.id.tv_title)).check(matches(isDisplayed()))
         onView(withId(R.id.tv_title)).check(matches(withText(dummyCourse[0].title)))
         onView(withId(R.id.tv_date)).check(matches(isDisplayed()))
@@ -34,27 +46,59 @@ class HomeActivityTest{
     }
 
     @Test
-    fun loadModule(){
-        onView(withId(R.id.rv_academy)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0,
-            click()))
+    fun loadModule() {
+        delayTwoSeconds()
+        onView(withId(R.id.rv_academy)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                0,
+                click()
+            )
+        )
+        delayTwoSeconds()
         onView(withId(R.id.btn_start)).perform(click())
+        delayTwoSeconds()
         onView(withId(R.id.rv_module)).check(matches(isDisplayed()))
     }
 
     @Test
-    fun loadDetailModule(){
-        onView(withId(R.id.rv_academy)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0,
-            click()))
+    fun loadDetailModule() {
+        delayTwoSeconds()
+        onView(withId(R.id.rv_academy)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                0,
+                click()
+            )
+        )
+        delayTwoSeconds()
         onView(withId(R.id.btn_start)).perform(click())
-        onView(withId(R.id.rv_module)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0,
-            click()))
+        delayTwoSeconds()
+        onView(withId(R.id.rv_module)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                0,
+                click()
+            )
+        )
+        delayTwoSeconds()
         onView(withId(R.id.web_view)).check(matches(isDisplayed()))
     }
 
     @Test
-    fun loadBookmark(){
+    fun loadBookmark() {
         onView(withText("Bookmark")).perform(click())
+        delayTwoSeconds()
         onView(withId(R.id.rv_bookmark)).check(matches(isDisplayed()))
-        onView(withId(R.id.rv_bookmark)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dummyCourse.size))
+        onView(withId(R.id.rv_bookmark)).perform(
+            RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
+                dummyCourse.size
+            )
+        )
+    }
+
+    private fun delayTwoSeconds() {
+        try {
+            Thread.sleep(2000)
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
     }
 }
