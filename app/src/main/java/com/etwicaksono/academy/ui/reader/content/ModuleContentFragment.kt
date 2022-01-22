75d1de6fc44d197c679aa54c63c18eca60732511
Eko Teguh Wicaksono <echotechno@rocketmail.com>
2022-01-22 10:19:12 +0700

    update ModuleContentFragment

--- app/src/main/java/com/etwicaksono/academy/ui/reader/content/ModuleContentFragment.kt
+++ app/src/main/java/com/etwicaksono/academy/ui/reader/content/ModuleContentFragment.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.etwicaksono.academy.data.source.local.entity.ModuleEntity
 import com.etwicaksono.academy.databinding.FragmentModuleContentBinding
 import com.etwicaksono.academy.ui.reader.CourseReaderViewModel
 import com.etwicaksono.academy.viewmodel.ViewModelFactory
+import com.etwicaksono.academy.vo.Status
 
 class ModuleContentFragment : Fragment() {
 
@@ -30,10 +32,26 @@ class ModuleContentFragment : Fragment() {
             val factory=ViewModelFactory.getInstance(requireActivity())
             val viewModel = ViewModelProvider(requireActivity(),factory)[CourseReaderViewModel::class.java]
 
-            fragmentModuleContentBinding.progressBar.visibility=View.VISIBLE
-            viewModel.getSelectedModule().observe(viewLifecycleOwner,{module->
-                fragmentModuleContentBinding.progressBar.visibility=View.GONE
-                if(module!=null)populateWebView(module)
+            viewModel.selectedModule.observe(viewLifecycleOwner,{moduleEntity->
+                if(moduleEntity!=null){
+                    when(moduleEntity.status){
+                        Status.LOADING->fragmentModuleContentBinding.progressBar.visibility=View.VISIBLE
+                        Status.SUCCESS->if(moduleEntity.data!=null){
+                            fragmentModuleContentBinding.progressBar.visibility=View.GONE
+                            if(moduleEntity.data.contentEntity!=null){
+                                populateWebView(moduleEntity.data)
+                            }
+                            setButtonNextPrevState(moduleEntity.data)
+                            if(!moduleEntity.data.read){
+                                viewModel.readContent(moduleEntity.data)
+                            }
+                        }
+                        Status.ERROR->{
+                            fragmentModuleContentBinding.progressBar.visibility=View.GONE
+                            Toast.makeText(context,"Terjadi kesalahan",Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
             })
         }
     }
@@ -41,6 +59,8 @@ class ModuleContentFragment : Fragment() {
     private fun populateWebView(module: ModuleEntity) {
         fragmentModuleContentBinding.webView.loadData(module.contentEntity?.content?:"","text/html","UTF-8")
     }
+
+    private fun setButtonNextPrevState(module:ModuleEntity) {}
 
     companion object {
         val TAG: String = ModuleContentFragment::class.java.simpleName
