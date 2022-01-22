75bfbe7861c5077e087eeb096aa2d7ea02aa4a3f
Eko Teguh Wicaksono <echotechno@rocketmail.com>
2022-01-22 09:15:58 +0700

    update AcademyFragment

--- app/src/main/java/com/etwicaksono/academy/ui/academy/AcademyFragment.kt
+++ app/src/main/java/com/etwicaksono/academy/ui/academy/AcademyFragment.kt
@@ -1,51 +1,67 @@
 package com.etwicaksono.academy.ui.academy
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.ViewModel
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.etwicaksono.academy.databinding.FragmentAcademyBinding
-import com.etwicaksono.academy.utils.DataDummy
 import com.etwicaksono.academy.viewmodel.ViewModelFactory
+import com.etwicaksono.academy.vo.Status
 
 
 class AcademyFragment : Fragment() {
 
-    private lateinit var binding: FragmentAcademyBinding
+    private var _binding: FragmentAcademyBinding? = null
+    private val binding get() = _binding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentAcademyBinding.inflate(layoutInflater, container, false)
-        return binding.root
+    ): View? {
+        _binding = FragmentAcademyBinding.inflate(layoutInflater, container, false)
+        return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         if (activity != null) {
-            val factory=ViewModelFactory.getInstance(requireActivity())
-            val viewModel=ViewModelProvider(this,factory)[AcademyViewModel::class.java]
+            val factory = ViewModelFactory.getInstance(requireActivity())
+            val viewModel = ViewModelProvider(this, factory)[AcademyViewModel::class.java]
             val academyAdapter = AcademyAdapter()
 
-            binding.progressBar.visibility=View.VISIBLE
-            viewModel.getCourses().observe(viewLifecycleOwner,{courses->
-                binding.progressBar.visibility=View.GONE
-                academyAdapter.setCourses(courses)
-                academyAdapter.notifyDataSetChanged()
+            viewModel.getCourses().observe(viewLifecycleOwner, { courses ->
+                if (courses != null) {
+                    when (courses.status) {
+                        Status.LOADING -> binding?.progressBar?.visibility = View.VISIBLE
+                        Status.SUCCESS -> {
+                            binding?.progressBar?.visibility = View.GONE
+                            academyAdapter.setCourses(courses.data)
+                            academyAdapter.notifyDataSetChanged()
+                        }
+                        Status.ERROR->{
+                             binding?.progressBar?.visibility = View.GONE
+                            Toast.makeText(context,"Terjadi kesalahan",Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
             })
 
-            with(binding.rvAcademy) {
+            binding?.rvAcademy?.apply {
                 layoutManager = LinearLayoutManager(context)
                 setHasFixedSize(true)
                 adapter = academyAdapter
             }
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
