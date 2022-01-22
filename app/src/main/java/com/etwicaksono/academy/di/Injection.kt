a114dfdb04733c818e1235c8461b08e60d5e2ce5
Eko Teguh Wicaksono <echotechno@rocketmail.com>
2022-01-22 07:53:22 +0700

    update Injection

--- app/src/main/java/com/etwicaksono/academy/di/Injection.kt
+++ app/src/main/java/com/etwicaksono/academy/di/Injection.kt
@@ -2,12 +2,18 @@ package com.etwicaksono.academy.di
 
 import android.content.Context
 import com.etwicaksono.academy.data.source.AcademyRepository
+import com.etwicaksono.academy.data.source.local.LocalDataSource
+import com.etwicaksono.academy.data.source.local.room.AcademyDatabase
 import com.etwicaksono.academy.data.source.remote.RemoteDataSource
+import com.etwicaksono.academy.utils.AppExecutors
 import com.etwicaksono.academy.utils.JsonHelper
 
 object Injection {
     fun provideRepository(context: Context): AcademyRepository {
+        val database=AcademyDatabase.getInstance(context)
         val remoteDataSource = RemoteDataSource.getInstance(JsonHelper(context))
-        return AcademyRepository.getInstance(remoteDataSource)
+        val localDataSource=LocalDataSource.getInstance(database.academyDao())
+        val appExecutors=AppExecutors()
+        return AcademyRepository.getInstance(remoteDataSource,localDataSource,appExecutors)
     }
 }
