52af1d403813a8644f3ae94f0124e69ca348ecda
Eko Teguh Wicaksono <echotechno@rocketmail.com>
2022-01-23 06:08:31 +0700

    Update AcademyViewModelTest

--- app/src/test/java/com/etwicaksono/academy/ui/academy/AcademyViewModelTest.kt
+++ app/src/test/java/com/etwicaksono/academy/ui/academy/AcademyViewModelTest.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.Observer
 import com.etwicaksono.academy.data.source.local.entity.CourseEntity
 import com.etwicaksono.academy.data.source.AcademyRepository
 import com.etwicaksono.academy.utils.DataDummy
+import com.etwicaksono.academy.vo.Resource
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
@@ -29,7 +30,7 @@ class AcademyViewModelTest {
     private lateinit var academyRepository: AcademyRepository
 
     @Mock
-    private lateinit var observer:Observer<List<CourseEntity>>
+    private lateinit var observer:Observer<Resource<List<CourseEntity>>>
 
     @Before
     fun setup(){
@@ -38,12 +39,12 @@ class AcademyViewModelTest {
 
     @Test
     fun getCourses() {
-        val dummyCourses = DataDummy.generateDummyCourses()
-        val courses = MutableLiveData<List<CourseEntity>>()
+        val dummyCourses = Resource.success(DataDummy.generateDummyCourses())
+        val courses = MutableLiveData<Resource<List<CourseEntity>>>()
         courses.value=dummyCourses
 
         `when`(academyRepository.getAllCourses()).thenReturn(courses)
-        val courseEntities=viewModel.getCourses().value
+        val courseEntities= viewModel.getCourses().value?.data
         verify(academyRepository).getAllCourses()
         assertNotNull(courseEntities)
         assertEquals(5,courseEntities?.size)
