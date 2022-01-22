fba5e1727b9b3c12279aac159dbc9c2229389463
Eko Teguh Wicaksono <echotechno@rocketmail.com>
2022-01-22 09:07:25 +0700

    update CourseReaderViewModel

--- app/src/main/java/com/etwicaksono/academy/ui/reader/CourseReaderViewModel.kt
+++ app/src/main/java/com/etwicaksono/academy/ui/reader/CourseReaderViewModel.kt
@@ -1,23 +1,73 @@
 package com.etwicaksono.academy.ui.reader
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
-import com.etwicaksono.academy.data.source.local.entity.ModuleEntity
 import com.etwicaksono.academy.data.source.AcademyRepository
+import com.etwicaksono.academy.data.source.local.entity.ModuleEntity
+import com.etwicaksono.academy.vo.Resource
 
-class CourseReaderViewModel(private val academyRepository:AcademyRepository) : ViewModel() {
-    private lateinit var courseId: String
-    private lateinit var moduleId: String
+class CourseReaderViewModel(private val academyRepository: AcademyRepository) : ViewModel() {
+    var courseId = MutableLiveData<String>()
+    var moduleId = MutableLiveData<String>()
 
     fun setSelectedCourse(courseId: String) {
-        this.courseId = courseId
+        this.courseId.value = courseId
     }
 
     fun setSelectedModule(moduleId: String) {
-        this.moduleId = moduleId
+        this.moduleId.value = moduleId
     }
 
-    fun getModules(): LiveData<List<ModuleEntity>> = academyRepository.getAllModulesByCourse(courseId)
+    var modules: LiveData<Resource<List<ModuleEntity>>> =
+        Transformations.switchMap(moduleId) { mCourseId ->
+            academyRepository.getAllModulesByCourse(mCourseId)
+        }
 
-    fun getSelectedModule(): LiveData<ModuleEntity> =academyRepository.getContent(courseId,moduleId)
+    var selectedModule: LiveData<Resource<ModuleEntity>> =
+        Transformations.switchMap(moduleId) { selectedPosition ->
+            academyRepository.getContent(selectedPosition)
+        }
+
+    fun readContent(module: ModuleEntity) {
+        academyRepository.setReadModule(module)
+    }
+
+    fun getModuleSize(): Int {
+        if (modules.value != null) {
+            val moduleEntities = modules.value?.data
+            if (moduleEntities != null) {
+                return moduleEntities.size
+            }
+        }
+        return 0
+    }
+
+    fun setNextPage() {
+        if (selectedModule.value != null && modules.value != null) {
+            val moduleEntity = selectedModule.value?.data
+            val moduleEntities = modules.value?.data
+            if (moduleEntity != null && moduleEntities != null) {
+                val position = moduleEntity.position
+                if (position < moduleEntities.size && position >= 0) {
+                    moduleId.value = moduleEntities[position + 1].moduleId
+                }
+
+            }
+        }
+    }
+
+    fun setPrevPage() {
+        if (selectedModule.value != null && modules.value != null) {
+            val moduleEntity = selectedModule.value?.data
+            val moduleEntities = modules.value?.data
+            if (moduleEntity != null && moduleEntities != null) {
+                val position = moduleEntity.position
+                if (position < moduleEntities.size && position >= 0) {
+                    moduleId.value = moduleEntities[position - 1].moduleId
+                }
+            }
+        }
+    }
 }
