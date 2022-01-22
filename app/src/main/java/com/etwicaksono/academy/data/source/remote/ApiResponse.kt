742f4fd1e25126a7fd187ea94112908488bf818d
Eko Teguh Wicaksono <echotechno@rocketmail.com>
2022-01-22 07:44:12 +0700

    setup NetworkBoundResource

--- app/src/main/java/com/etwicaksono/academy/data/source/remote/ApiResponse.kt
+++ app/src/main/java/com/etwicaksono/academy/data/source/remote/ApiResponse.kt
@@ -3,8 +3,9 @@ package com.etwicaksono.academy.data.source.remote
 class ApiResponse<T>(val status:StatusResponse,val body:T, val message:String?) {
     companion object{
         fun <T> success(body:T):ApiResponse<T> = ApiResponse(StatusResponse.SUCCESS,body,null)
-        fun <T> empty(msg:String,body:T):ApiResponse<T> = ApiResponse(StatusResponse.EMPTY,body,msg)
-        fun <T> error(msg:String,body:T):ApiResponse<T> = ApiResponse(StatusResponse.ERROR,body,msg)
 
+        fun <T> empty(msg:String,body:T):ApiResponse<T> = ApiResponse(StatusResponse.EMPTY,body,msg)
+
+        fun <T> error(msg:String,body:T):ApiResponse<T> = ApiResponse(StatusResponse.ERROR,body,msg)
     }
 }
