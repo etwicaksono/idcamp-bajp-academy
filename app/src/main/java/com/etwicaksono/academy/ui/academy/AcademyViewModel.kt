aeae470ff5e3338c8436bbfb5a0e2985abe19614
Eko Teguh Wicaksono <echotechno@rocketmail.com>
2022-01-23 09:22:20 +0700

    update AcademyViewModel

--- app/src/main/java/com/etwicaksono/academy/ui/academy/AcademyViewModel.kt
+++ app/src/main/java/com/etwicaksono/academy/ui/academy/AcademyViewModel.kt
@@ -2,10 +2,11 @@ package com.etwicaksono.academy.ui.academy
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.paging.PagedList
 import com.etwicaksono.academy.data.source.local.entity.CourseEntity
 import com.etwicaksono.academy.data.source.AcademyRepository
 import com.etwicaksono.academy.vo.Resource
 
 class AcademyViewModel(private val academyRepository:AcademyRepository) : ViewModel() {
-    fun getCourses(): LiveData<Resource<List<CourseEntity>>> = academyRepository.getAllCourses()
+    fun getCourses(): LiveData<Resource<PagedList<CourseEntity>>> = academyRepository.getAllCourses()
 }
