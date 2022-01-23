61783d66b89d507b6585f3fc78a4891a88d5a7f7
Eko Teguh Wicaksono <echotechno@rocketmail.com>
2022-01-23 09:10:03 +0700

    update LocalDataSource

--- app/src/main/java/com/etwicaksono/academy/data/source/local/LocalDataSource.kt
+++ app/src/main/java/com/etwicaksono/academy/data/source/local/LocalDataSource.kt
@@ -1,6 +1,7 @@
 package com.etwicaksono.academy.data.source.local
 
 import androidx.lifecycle.LiveData
+import androidx.paging.DataSource
 import com.etwicaksono.academy.data.source.local.entity.CourseEntity
 import com.etwicaksono.academy.data.source.local.entity.CourseWithModule
 import com.etwicaksono.academy.data.source.local.entity.ModuleEntity
@@ -8,9 +9,9 @@ import com.etwicaksono.academy.data.source.local.room.AcademyDao
 
 class LocalDataSource private constructor(private val mAcademyDao: AcademyDao) {
 
-    fun getAllCourses(): LiveData<List<CourseEntity>> = mAcademyDao.getCourses()
+    fun getAllCourses(): DataSource.Factory<Int, CourseEntity> = mAcademyDao.getCourses()
 
-    fun getBookmarkedCourses(): LiveData<List<CourseEntity>> = mAcademyDao.getBookmarkedCourse()
+    fun getBookmarkedCourses(): DataSource.Factory<Int, CourseEntity> = mAcademyDao.getBookmarkedCourse()
 
     fun getCourseWithModules(courseId: String): LiveData<CourseWithModule> =
         mAcademyDao.getCourseWithModuleById(courseId)
