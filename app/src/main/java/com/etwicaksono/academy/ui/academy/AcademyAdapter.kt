3828e6951af82191a19015daba9486d620e90fcf
Eko Teguh Wicaksono <echotechno@rocketmail.com>
2022-01-23 09:26:23 +0700

    update AcademyAdapter

--- app/src/main/java/com/etwicaksono/academy/ui/academy/AcademyAdapter.kt
+++ app/src/main/java/com/etwicaksono/academy/ui/academy/AcademyAdapter.kt
@@ -3,6 +3,8 @@ package com.etwicaksono.academy.ui.academy
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.paging.PagedListAdapter
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
@@ -11,7 +13,33 @@ import com.etwicaksono.academy.data.source.local.entity.CourseEntity
 import com.etwicaksono.academy.databinding.ItemsAcademyBinding
 import com.etwicaksono.academy.ui.detail.DetailCourseActivity
 
-class AcademyAdapter : RecyclerView.Adapter<AcademyAdapter.CourseViewHolder>() {
+class AcademyAdapter : PagedListAdapter<CourseEntity,AcademyAdapter.CourseViewHolder>(DIFF_CALLBACK){
+
+    private val listCourses = ArrayList<CourseEntity>()
+
+    fun setCourses(courses: List<CourseEntity>?) {
+        if (courses == null) return
+        this.listCourses.clear()
+        this.listCourses.addAll(courses)
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): AcademyAdapter.CourseViewHolder {
+        val itemsAcademyBinding =
+            ItemsAcademyBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return CourseViewHolder(itemsAcademyBinding)
+    }
+
+    override fun onBindViewHolder(holder: AcademyAdapter.CourseViewHolder, position: Int) {
+        val course = listCourses[position]
+        holder.bind(course)
+    }
+
+    override fun getItemCount(): Int = listCourses.size
+
+
     inner class CourseViewHolder(private val binding: ItemsAcademyBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(course: CourseEntity) {
@@ -38,28 +66,16 @@ class AcademyAdapter : RecyclerView.Adapter<AcademyAdapter.CourseViewHolder>() {
 
     }
 
-    private val listCourses = ArrayList<CourseEntity>()
+    companion object{
+        private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<CourseEntity>() {
+            override fun areItemsTheSame(oldItem: CourseEntity, newItem: CourseEntity): Boolean {
+                return oldItem.courseId==newItem.courseId
+            }
 
-    fun setCourses(courses: List<CourseEntity>?) {
-        if (courses == null) return
-        this.listCourses.clear()
-        this.listCourses.addAll(courses)
+            override fun areContentsTheSame(oldItem: CourseEntity, newItem: CourseEntity): Boolean {
+                return oldItem==newItem
+            }
+        }
     }
-
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): AcademyAdapter.CourseViewHolder {
-        val itemsAcademyBinding =
-            ItemsAcademyBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return CourseViewHolder(itemsAcademyBinding)
-    }
-
-    override fun onBindViewHolder(holder: AcademyAdapter.CourseViewHolder, position: Int) {
-        val course = listCourses[position]
-        holder.bind(course)
-    }
-
-    override fun getItemCount(): Int = listCourses.size
 
 }
