4cc0d57d87bb9f18475ce78511af1698f319a259
Eko Teguh Wicaksono <echotechno@rocketmail.com>
2022-01-23 09:31:37 +0700

    update BookmarkAdapter

--- app/src/main/java/com/etwicaksono/academy/ui/bookmark/BookmarkAdapter.kt
+++ app/src/main/java/com/etwicaksono/academy/ui/bookmark/BookmarkAdapter.kt
@@ -3,6 +3,8 @@ package com.etwicaksono.academy.ui.bookmark
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.paging.PagedListAdapter
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
@@ -12,7 +14,31 @@ import com.etwicaksono.academy.databinding.ItemsBookmarkBinding
 import com.etwicaksono.academy.ui.detail.DetailCourseActivity
 
 class BookmarkAdapter(private val callback: BookmarkFragmentCallback) :
-    RecyclerView.Adapter<BookmarkAdapter.CourseViewHolder>() {
+    PagedListAdapter<CourseEntity,BookmarkAdapter.CourseViewHolder>(DIFF_CALLBACK) {
+
+    private val listCourses=ArrayList<CourseEntity>()
+
+    fun setCourses(courses:List<CourseEntity>?){
+        if(courses==null) return
+        this.listCourses.clear()
+        this.listCourses.addAll(courses)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CourseViewHolder {
+        val itemsBookmarkBinding=ItemsBookmarkBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return CourseViewHolder(itemsBookmarkBinding)
+    }
+
+    override fun onBindViewHolder(holder: CourseViewHolder, position: Int) {
+        val course=listCourses[position]
+        holder.bind(course)
+    }
+
+    override fun getItemCount(): Int =listCourses.size
+
+    fun getSwipedData(swipedPosition:Int):CourseEntity?=getItem(swipedPosition)
+
+
     inner class CourseViewHolder(private val binding: ItemsBookmarkBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(course: CourseEntity) {
@@ -37,24 +63,16 @@ class BookmarkAdapter(private val callback: BookmarkFragmentCallback) :
         }
     }
 
-    private val listCourses=ArrayList<CourseEntity>()
+    companion object{
+        private val DIFF_CALLBACK= object :DiffUtil.ItemCallback<CourseEntity>() {
+            override fun areItemsTheSame(oldItem: CourseEntity, newItem: CourseEntity): Boolean {
+                return oldItem.courseId==newItem.courseId
+            }
 
-    fun setCourses(courses:List<CourseEntity>?){
-        if(courses==null) return
-        this.listCourses.clear()
-        this.listCourses.addAll(courses)
+            override fun areContentsTheSame(oldItem: CourseEntity, newItem: CourseEntity): Boolean {
+                return oldItem==newItem
+            }
+
+        }
     }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CourseViewHolder {
-        val itemsBookmarkBinding=ItemsBookmarkBinding.inflate(LayoutInflater.from(parent.context),parent,false)
-        return CourseViewHolder(itemsBookmarkBinding)
-    }
-
-    override fun onBindViewHolder(holder: CourseViewHolder, position: Int) {
-        val course=listCourses[position]
-        holder.bind(course)
-    }
-
-    override fun getItemCount(): Int =listCourses.size
-
 }
